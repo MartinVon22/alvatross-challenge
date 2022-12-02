ed1f2c58c299910c0c80a299830f0071abdc4733
Martin <83525340+MartinVon22@users.noreply.github.com>
2022-12-02 15:09:36 -0300

    added service for get rooms of plant by id

--- src/core/services/roomsService.tsx
+++ src/core/services/roomsService.tsx
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { Plant } from '../models/plant';
+import { Room } from '../models/room';
 
 const URL_ENDPOINT = "https://apimocha.com/alvatross-backend/";
 
-export default function getPlants() {
+export function getPlants() {
     return axios.get<Plant[]>(`${URL_ENDPOINT}/plants`);
+}
+
+export function getRooms(plantId: number) {
+    return axios.get<Room[]>(`${URL_ENDPOINT}/plant/${plantId}/rooms`);
 }
