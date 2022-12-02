5e1adbdd7b3c2c307741acf6eaefd83720bd068c
Martin <83525340+MartinVon22@users.noreply.github.com>
2022-12-01 21:29:05 -0300

    created option input and box for rooms

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,6 +1,18 @@
+import './Home.css';
+
 export const Home = () => {
 
     return (
-        <p>Bienvenido a la página principal</p>
+        <div className="homeContainer">
+            <div className="header">
+                <h1>Salas</h1>
+                <select name="" id="">
+                    <option value="1">Planta 1</option>
+                    <option value="1">Planta 1</option>
+                </select>
+            </div>
+            <div className="boxRooms">
+            </div>
+        </div>
     )
 }
